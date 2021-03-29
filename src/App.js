df5b52b5712320efe3297c75399cf2622f4a10d3
Peter Son <peteson.me@gmail.com>
2021-03-30 07:47:33 +1300

    api call with effect hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
+import React, { useState, useEffect, useRef } from 'react';
+import Dashboard from "./components/Dashboard";
 import './App.css';
 
 function App() {
+  const [ currencies, setCurrencies ] = useState([]);
+  const [ pair, setPair ] = useState('');
+  const [ price, setPrice ] = useState('0.00');
+  const [ pastData, setPastData ] = useState({});
+  const ws = useRef(null);
+
+  let first = useRef(false);
+  const url = 'https://api.pro.coinbase.com';
+
+  useEffect(() => {
+    ws.current = new WebSocket("wss://ws-feed.pro.coinbase.com");
+
+    let pairs = [];
+
+    const apiCall = async () => {
+      await fetch(url + "/products")
+        .then((res) => res.json())
+        .then((data) => (pairs = data));
+      console.log("PAIRS: ", pairs);
+      let filtered = pairs.filter((pair) => {
+        if (pair.quote_currency === "NZD") {
+          return pair;
+        }
+      });
+
+      filtered = filtered.sort((a, b) => {
+        if (a.base_currency < b.base_currency) {
+          return -1;
+        }
+        if (a.base_currency > b.base_currency) {
+          return 1;
+        }
+        return 0;
+      });
+
+      console.log("FILTERED: ", filtered);
+      setCurrencies(filtered);
+
+      first.current = true;
+    };
+
+    apiCall();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <p>Hello world</p>
+        <Dashboard></Dashboard>
       </header>
     </div>
   );
